de784846b6d45925871ea801ce512b8abd47d95e
= <=>
2021-12-15 16:48:22 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+//Only load dotenv if we're not in production environment
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
+//Default router
+const indexRouter = require("./routes/index");
+const scoreKingRouter = require("./routes/score-king");
+const scoreKingPrivacyRouter = require("./routes/privacy");
+// const bookRouter = require("./routes/books");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout"); //Every file will use this as an HTML template
+app.use(expressLayouts);
+app.use(methodOverride("_method"));
+app.use(express.static("public")); //Style sheets, js files, images
+
+// Parses body to json
+app.use(express.json());
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+// const mongoose = require("mongoose");
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+// const db = mongoose.connection;
+// db.on("error", (error) => console.error(error));
+// db.once("open", () => console.log("Connected to Mongoose"));
+
+//Route to default router
+app.use("/", indexRouter);
+app.use("/score-king", scoreKingRouter);
+app.use("/score-king/privacy", scoreKingPrivacyRouter);
+// app.use("/books", bookRouter);
+
+app.listen(process.env.PORT || 3003);
